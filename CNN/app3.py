5ec87842dc6fbe1463398a5f34813dd6d29be5ec
WFT08 <wft08@WFT08s-MacBook-Pro.local>
2025-08-21 11:17:38 +0530

    updated .gitignore

--- CNN/app3.py
+++ CNN/app3.py
@@ -323,5 +323,9 @@ def chat():
     result = router.generate(prompt_full)
     return jsonify({"answer": result})
 
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
